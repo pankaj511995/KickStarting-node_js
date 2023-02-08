e27eda5e71982832e80cb79962eec822924ea754
pankaj511995 <kpankaj511995@gmail.com>
2023-02-08 20:28:35 +0530

    print_above_text

--- write.js
+++ write.js
@@ -2,38 +2,42 @@ const http1= require('http')
 console.log('server started')
 const fs=require('fs')
 const server=http1.createServer((req,res)=>{
-    // try{
+   
 let res_url=req.url;
 const methodres=req.method;
 console.log(res_url, methodres)
+
+let print=fs.readFileSync('text.txt').toString()
+console.log(print)
 res.setHeader('Content-type','text/html')
 if(res_url==='/'){
   
     res.write(`<hrml>
 <head><title>this is node.js</title></head>
-   <body> <form action="/sent" method="POST"> <input type="text" name="message"><button>Send</button></input></form> </body>
+   <body><div>${print}</div>
+    <form action="/send" method="POST"> <input type="text" name="message"><button>Send</button></input></form> </body>
    </hrml>`)
      res.end();
     }
-if(res_url==='/sent' && methodres==='POST'){
+if(res_url==='/send' && methodres==='POST'){
     const gotsend=[];
-    req.on('data',(chunk)=>{
-        console.log(chunk);
-        gotsend.push(chunk)
-    });
-    req.on('end',()=>{
+    req.on('data',(chunk)=>gotsend.push(chunk));
+     req.on('end',()=>{
         console.log(gotsend)
-        console.log(' i am string')
         const result=Buffer.concat(gotsend).toString().split('=')
         const message=result[1]
         fs.writeFileSync('text.txt',message,()=>{
             res.statusCode=302
         res.setHeader('Location','/')
+        // res.redirect('/')
+         res.end()
         })
-        return res.end()
+       
       
     });
     
 };
 })
-server.listen(3000)
+// node write.js  
+server.listen(8000)
+
